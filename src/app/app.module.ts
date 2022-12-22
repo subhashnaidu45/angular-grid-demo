96c0d9d0024d8048d8cf9b7f1bfcef263980f976
subhashnaidu45 <117711902+subhashnaidu45@users.noreply.github.com>
2022-12-22 13:02:34 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+import { GridDemoComponent } from './grid-demo/grid-demo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatSelectModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+  ],
+  declarations: [AppComponent, GridDemoComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/grid-demo/grid-demo.component.ts
+++ src/app/grid-demo/grid-demo.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+export interface PeriodicElement {
+  name: string;
+  gender: string;
+  skills: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { name: 'Hydrogen', gender: 'male', skills: 'H' },
+  { name: 'Helium', gender: 'male', skills: 'He' },
+  { name: 'Lithium', gender: 'male', skills: 'Li' },
+  { name: 'Beryllium', gender: 'male', skills: 'Be' },
+];
+
 @Component({
   selector: 'app-grid-demo',
   templateUrl: './grid-demo.component.html',
   styleUrls: ['./grid-demo.component.css'],
 })
 export class GridDemoComponent implements OnInit {
-  toppings = new FormControl('');
-  toppingList: string[] = [
-    'Extra cheese',
-    'Mushroom',
-    'Onion',
-    'Pepperoni',
-    'Sausage',
-    'Tomato',
-  ];
+  skills = new FormControl('');
+  skillsList: string[] = ['Angular', 'Dotnet', 'Python'];
   constructor() {}
-
+  displayedColumns: string[] = ['name', 'gender', 'skills'];
+  dataSource = ELEMENT_DATA;
   ngOnInit() {}
 }
